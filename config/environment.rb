7c8c38d86bba4f41b09df9412607405daa33cae5
Chad McKenna <cmckenna@mines.edu>
2011-05-24 12:03:19 -0600

    Quick change to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   config.gem 'paperclip'
+  config.gem 'sass'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
