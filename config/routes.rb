9f504f1c959722f0b78bacb3d26d42dc43416bd4
Jerel Miller <jermille@mines.edu>
2011-05-24 11:37:11 -0600

    Added authlogic stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  map.signup 'signup', :controller => 'users', :action => 'new'
+  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
+  map.login 'login', :controller => 'user_sessions', :action => 'new'
+  map.resources :user_sessions
+
+  map.resources :users
+
   map.resources :requests
 
   # The priority is based upon order of creation: first created -> highest priority.
