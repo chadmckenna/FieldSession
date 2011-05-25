aaa5277ad57e69a538479a0e3e5800af4c9dd90c
Ben Makuh <bmakuh@mines.edu>
2011-05-25 10:40:14 -0600

    updated application_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :children
-
   map.resources :households
-
-  map.resources :roles
+  map.resources :requests
 
   map.signup 'signup', :controller => 'users', :action => 'new'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
   map.login 'login', :controller => 'user_sessions', :action => 'new'
-  map.resources :user_sessions
+  map.resources :user_sessions, :only => [:new, :create, :destroy]
+  map.resources :users, :only => [:new, :create]
+  
+  map.root :requests
 
   map.namespace :admin do |admin|
     admin.resources :roles
@@ -16,21 +17,16 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :children
     admin.resources :households
     admin.resources :requests
+    admin.root :controller => 'admin', :action => 'index'
   end
 
   map.namespace :members do |members|
-    members.resources :roles
-    members.resources :users
+    members.resources :users, :only => [:show, :edit, :update]
     members.resources :children
     members.resources :households
     members.resources :requests
+    members.root :controller => 'members', :action => 'index'
   end
-
-  map.resources :users
-
-  map.resources :requests
-  
-  map.root :requests
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
