105ee5be0040e00049d7e428f87a2452b09411ca
Chad McKenna <cmckenna@mines.edu>
2011-05-25 13:04:51 -0600

    minor changes

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,18 @@
 class Role < ActiveRecord::Base
   attr_accessible :name, :description, :users_count
   
+  validates_length_of :name, :minimum => 1
+  validates_uniqueness_of :name
+  
   has_many :users
+  
+  def to_s
+    self.name
+  end
+  
+  protected
+    
+    def after_initialize
+      self.name = name.downcase.strip unless name.nil?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,37 @@ class User < ActiveRecord::Base
   belongs_to :household
   belongs_to :role
   
+  before_validation_on_create :assign_default_role
+  
+  def is?(role_symbol)
+    role_symbols.include? role_symbol
+  end
+  
   def is_admin?
     role_symbols.include?(:administrator) || role_symbols.include?(:developer)
+  end
+  
+  def is_member?
+    role_symbols.include?(:member)
   end
   
   def role_symbols
     [role.name.downcase.to_sym]
   end
   
-  private
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def to_s
+    self.full_name
+  end
+  
+  protected
+
+    def validate
+      errors.add_to_base "User must have a role." if self.role.nil?
+    end
 
     def assign_default_role
       self.role = Role.find_by_name('member') if role_id.blank?
