3fdea88d73391c29e4a7301e8169a7f2631c74f3
Jerel Miller <jermille@mines.edu>
2011-05-25 11:06:35 -0600

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def google_webfont_stylesheet(face)
+    "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://fonts.googleapis.com/css?family=#{face}\" />"
+  end
 end
