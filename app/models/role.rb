1935e8d685d2d8db453e855913b9bc56f68840b5
Ben Makuh <bmakuh@mines.edu>
2011-05-25 11:36:47 -0600

    added association between user and role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ActiveRecord::Base
   attr_accessible :name, :description, :users_count
+  
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   belongs_to :household
+  belongs_to :role
 end
