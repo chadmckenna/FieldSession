c78b7f1912ca80ba5017bd3140448e433002cbc8
Chad McKenna <cmckenna@mines.edu>
2011-05-26 15:05:20 -0600

    Made a bunch of changes, mainly some cosmetic changes to requests and sass

--- app/models/household.rb
+++ app/models/household.rb
@@ -4,4 +4,7 @@ class Household < ActiveRecord::Base
   has_many :children
   has_many :users
 
+  def to_s
+    self.name
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -6,9 +6,9 @@ class Request < ActiveRecord::Base
 
   validates_presence_of :cost
   validates_numericality_of :cost
-  validates_is_after :date
-  validates_is_after :start_time
-  validates_is_after :end_time, :after => :start_time
+  #validates_is_after :date
+  #validates_is_after :start_time
+  #validates_is_after :end_time, :after => :start_time
 
 end
 
