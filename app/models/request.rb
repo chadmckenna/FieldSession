18b61f0177cf3359845cefd3165e23b7aa56a981
Ben Makuh <bmakuh@mines.edu>
2011-05-26 15:06:44 -0600

    some stuff

--- app/models/request.rb
+++ app/models/request.rb
@@ -11,10 +11,23 @@ class Request < ActiveRecord::Base
   validates_is_after :end_time, :after => :start_time
 
   before_create :assign_household
+  before_create :update_times_to_correct_date
 
   protected
     def assign_household
       self.household = @user.household if self.household.nil?
+    end
+
+    def update_times_to_correct_date
+      start_time.year = date.year
+      start_time.month = date.month
+      start_time.day = date.day
+      start_time.save
+
+      end_time.year = date.year
+      end_time.month = date.month
+      end_time.day = date.day
+      end_time.save
     end
 end
 
