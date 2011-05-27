7d496e34b198152c34981231350aab4e764bac30
Ben Makuh <bmakuh@mines.edu>
2011-05-27 12:34:38 -0600

    your own requests don't show up in the request stream anymore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,12 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#   
+#
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
-Role.create!(:name => 'developer', :description => 'The system developer. There is no higher access level.')
-Role.create!(:name => 'administrator', :description => 'A system administrator. Full access to all features.')
-Role.create!(:name => 'member', :description => 'A typical application user.')
+#Role.create!(:name => 'developer', :description => 'The system developer. There is no higher access level.')
+#Role.create!(:name => 'administrator', :description => 'A system administrator. Full access to all features.')
+#Role.create!(:name => 'member', :description => 'A typical application user.')
+
+User.create!(:username => "bmakuh", :email => "bmakuh@mines.edu", :password => "changeme", :password_confirmation => "changeme", :first_name => "Ben", :last_name => "Makuh", :household => Household.new(:name => "Makuh", :credits => 2000))
+
