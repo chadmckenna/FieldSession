c1c15bb7d8349d032227544f645112ff3dfa7132
Chad McKenna <cmckenna@mines.edu>
2011-06-01 15:14:42 -0600

    Made changes to Child model and views and controllers.

--- app/controllers/members/children_controller.rb
+++ app/controllers/members/children_controller.rb
@@ -1,11 +1,15 @@
 class Members::ChildrenController < Members::MembersController
     
   def index
-    @children = Child.find_by_household(current_user.household)
+    @children = Child.find_all_by_household_id(current_user.household_id)
   end
 
   def show
     @child = Child.find(params[:id])
+    unless @child.household_id.eql? current_user.household_id
+      flash[:error] = "You do not have permission to view the details of this child."
+      redirect_to members_children_path
+    end
   end
 
   def new
@@ -26,13 +30,20 @@ class Members::ChildrenController < Members::MembersController
 
   def edit
     @child = Child.find(params[:id])
+    unless @child.household_id.eql? current_user.household_id
+      flash[:error] = "You do not have permission to edit the details of this child."
+      redirect_to members_children_path
+    end
   end
 
   def update
     @child = Child.find(params[:id])
-    if @child.update_attributes(params[:child])
+    if !@child.household_id.eql? current_user.household_id
+      flash[:error] = "You do not have permission to view the details of this child."
+      redirect_to members_children_path
+    elsif @child.update_attributes(params[:child])
       flash[:notice] = "Successfully updated child."
-      redirect_to @child
+      redirect_to members_child_path(@child)
     else
       render :action => 'edit'
     end
@@ -42,7 +53,7 @@ class Members::ChildrenController < Members::MembersController
     @child = Child.find(params[:id])
     @child.destroy
     flash[:notice] = "Successfully destroyed child."
-    redirect_to children_url
+    redirect_to members_children_path
   end
   
 end
