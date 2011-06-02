ad1e6e1d4f2c6c732dbcc29b11f4b3430802df9f
Jerel Miller <jermille@mines.edu>
2011-06-02 10:38:08 -0600

    Changed database schema for neighbor

--- db/migrate/20110602160622_create_neighbors.rb
+++ db/migrate/20110602160622_create_neighbors.rb
@@ -2,7 +2,7 @@ class CreateNeighbors < ActiveRecord::Migration
   def self.up
     create_table :neighbors do |t|
       t.integer :user_id
-      t.integer :neighbor_id
+      t.integer :user_id_neighbor
       t.timestamps
     end
   end
