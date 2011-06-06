c24d707359ed64e3fa1049694344ff3c3a6ba0e5
Ben Makuh <bmakuh@mines.edu>
2011-06-06 14:15:30 -0600

    fancy search bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,12 @@ module ApplicationHelper
     render :partial => 'layouts/admin_menu_items' if user.is_admin?
   end
   
+  def default_text(text)  
+     onFocusFunction = "field = event.target || event.srcElement; if (field.value=='#{text}') {field.value = '';}else {return false}"  
+     onBlurFunction = "field = event.target || event.srcElement; if (field.value=='') {field.value = '#{text}';}else {return false}"  
+     {:value => text, :onfocus => onFocusFunction, :onblur => onBlurFunction}   
+  end 
+  
   module ActionView
     module Helpers
       module DateHelper
