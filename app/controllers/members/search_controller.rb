07659d39debc0a92694826b42427600f92a26e76
Chad McKenna <cmckenna@mines.edu>
2011-06-06 14:46:44 -0600

    Doesn't search for your household

--- app/controllers/members/search_controller.rb
+++ app/controllers/members/search_controller.rb
@@ -1,6 +1,10 @@
 class Members::SearchController < Members::MembersController
   
   def index
-    @households = Household.search(params[:search])
+    @households = []
+    households = Household.search(params[:search])
+    for household in households do
+      @households << household if household.id != current_user.household_id
+    end
   end
 end
