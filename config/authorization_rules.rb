32852addb2501e99dd7631bd1b1be91d39036a0e
Ben Makuh <bmakuh@mines.edu>
2011-06-06 13:39:00 -0600

    100% wide search, views for children and caregivers from household

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -27,7 +27,7 @@ authorization do
 end
 
 privileges do
-  privilege :manage, :includes => [:create, :read, :update, :delete]
+  privilege :manage, :includes => [:create, :read, :update, :delete, :show]
   privilege :read, :includes => [:index, :show]
   privilege :create, :includes => :new
   privilege :update, :includes => [:edit]
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(:version => 20110605093349) do
   end
 
   create_table "children_requests", :id => false, :force => true do |t|
-    t.integer "child_id"
-    t.integer "request_id"
+    t.integer  "child_id"
+    t.integer  "request_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "hidden_requests", :force => true do |t|
