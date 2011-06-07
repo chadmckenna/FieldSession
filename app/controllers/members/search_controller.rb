c88d006be50aaf143cddfd10987144afdb64bcd7
Jerel Miller <jermille@mines.edu>
2011-06-07 16:52:54 -0600

    fixed heroku search case sensitivity

--- app/controllers/members/search_controller.rb
+++ app/controllers/members/search_controller.rb
@@ -1,8 +1,12 @@
 class Members::SearchController < Members::MembersController
   
   def index
+    params[:search].downcase
     @households = Household.search(params[:search])
     @households.delete_if { |household| household.id.eql? current_user.household_id } unless @households.eql? nil
+    for household in @households
+      household.name.downcase
+    end
     @users = User.search(params[:search])
     @users.delete_if { |user| user.id.eql? current_user.id} unless @users.eql? nil
   end
