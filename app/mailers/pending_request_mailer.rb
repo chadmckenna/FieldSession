b791f6d1dfc738b5487a17e74612ae7cf50badaa
tmesquita <tmesquit@mines.edu>
2011-06-09 11:53:55 -0600

    updated email notifications

--- app/mailers/pending_request_mailer.rb
+++ app/mailers/pending_request_mailer.rb
@@ -2,23 +2,27 @@ class PendingRequestMailer < ActionMailer::Base
 
 	def request_volunteered_email(pending_request)
 		@users = User.find(:all, :conditions => {:household_id => pending_request.belongs_to_household_id})
+		volunteer = User.find(:all, :conditions => {:id => pending_request.caregiver_requestor_id})
+		request = Request.find(pending_request.request_id)
+			
 		for user in @users
 			subject		"Your request has a volunteer"
 			from       	"Villages <do-not-reply@fieldsession.heroku.com>"
 			recipients	user.email
 			sent_on		Time.now
-			body		("testing")	
+			body		(:caregiver => volunteer, :request_title => request.title, :request_date => request.from_date, :request_time => request.start_time)	
 		end
 	end
 
 	def volunteer_confirmed_email(pending_request)
-		@users = User.find(:all, :conditions => {:household_id => pending_request.household_commit_id})
-		for user in @users
-			from       	"Villages <do-not-reply@fieldsession.heroku.com>"
-			subject		"Your request to volunteer to watch //so and so's kids has been confirmed"
-			recipients	user.email
-			sent_on		Time.now
-			body		("testing")	
-		end
+		user = User.find(pending_request.caregiver_commit_id)
+		request = Request.find(pending_request.request_id)
+		household = Household.find(pending_request.belongs_to_household_id)
+		from       	"Villages <do-not-reply@fieldsession.heroku.com>"
+		subject		"Your request to volunteer to watch //so and so's kids has been confirmed"
+		recipients	user.email
+		sent_on		Time.now
+		body		(:household_name => household.name, :request_title => request.title, :request_date => request.from_date, :request_time => request.start_time)
+		
 	end
 end
