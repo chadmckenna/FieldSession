e48c9229bba382ec140012b30016841ea6327b24
Chad McKenna <cmckenna@mines.edu>
2011-06-13 12:13:01 -0600

    Modified migration so that it works with postgres

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110610185858) do
+ActiveRecord::Schema.define(:version => 20110613170636) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street1",      :null => false
@@ -127,7 +127,7 @@ ActiveRecord::Schema.define(:version => 20110610185858) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "household_id"
-    t.boolean  "household_confirmed", :limit => 255, :default => false
+    t.boolean  "household_confirmed", :default => false
   end
 
 end
