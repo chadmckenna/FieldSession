0a06dc37522a57233e361ebadbcef1457ae1d495
Jerel Miller <jermille@mines.edu>
2011-06-13 15:32:51 -0600

    added pending_requests to notifications

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110613170636) do
+ActiveRecord::Schema.define(:version => 20110613212925) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street1",      :null => false
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(:version => 20110613170636) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "belongs_to_household_id"
+    t.string   "read"
   end
 
   create_table "requests", :force => true do |t|
