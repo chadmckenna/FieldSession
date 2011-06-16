a0c1745dfe743d503b97891b279a8bd35b1bade5
Chad McKenna <cmckenna@mines.edu>
2011-06-15 18:16:54 -0600

    Changes to voluteer requests

--- app/controllers/members/pending_requests_controller.rb
+++ app/controllers/members/pending_requests_controller.rb
@@ -10,24 +10,29 @@ class Members::PendingRequestsController < Members::MembersController
   end
   
   def create
-    @pending_request = PendingRequest.new
-    @pending_request.request_id = params[:request_id]
-    @pending_request.caregiver_requestor_id = current_user.id
-    @pending_request.belongs_to_household_id = params[:belongs_to_household_id]
-    @pending_request.read = false
-    
-    if @pending_request.save
-      @household = Household.find(:all, :conditions => {:id => @pending_request.belongs_to_household_id})
-      @users = User.find(:all, :conditions => {:household_id => @pending_request.belongs_to_household_id, :household_confirmed => true})
-      #puts @household#.name + " ASDADA"
-      @pending_request.send_request_volunteered_email(@users)
-      flash[:success] = "You have successfully volunteered for this request"
-      redirect_to members_requests_path
+    current_pending_requests = PendingRequest.find(:all, :conditions => {:request_id => params[:request_id], :caregiver_requestor_id => current_user.id})
+    if current_pending_requests.count.eql? 0 
+      @pending_request = PendingRequest.new
+      @pending_request.request_id = params[:request_id]
+      @pending_request.caregiver_requestor_id = current_user.id
+      @pending_request.belongs_to_household_id = params[:belongs_to_household_id]
+      @pending_request.read = false
+      
+      if @pending_request.save
+        @household = Household.find(:all, :conditions => {:id => @pending_request.belongs_to_household_id})
+        @users = User.find(:all, :conditions => {:household_id => @pending_request.belongs_to_household_id, :household_confirmed => true})
+        #puts @household#.name + " ASDADA"
+        @pending_request.send_request_volunteered_email(@users)
+        flash[:success] = "You have successfully volunteered for this request"
+        redirect_to members_requests_path
+      else
+        flash[:error] = "There were errors volunteering for this request"
+        redirect_to members_requests_path
+      end
     else
-      flash[:error] = "There were errors volunteering for this request"
+      flash[:error] = "You cannot volunteer for the same request twice."
       redirect_to members_requests_path
     end
-      
   end
 
   def show
