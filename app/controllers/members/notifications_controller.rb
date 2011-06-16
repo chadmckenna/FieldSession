031fb7bf15b1941b42221ff5b5b2845e1006249f
tmesquita <tmesquit@mines.edu>
2011-06-16 15:36:43 -0600

    notification for caregiver requests added

--- app/controllers/members/notifications_controller.rb
+++ app/controllers/members/notifications_controller.rb
@@ -3,7 +3,7 @@ class Members::NotificationsController < Members::MembersController
   def update_read
     @neighbor_requests_confirmed = Neighbor.find(:all, :conditions => {:household_id => current_user.household.id, :household_confirmed => "t", :neighbor_confirmed => "t", :read => "f"})
     @pending_requests_confirmed =  PendingRequest.find(:all, :conditions => {:caregiver_commit_id => current_user.id, :confirmed => "true", :read => 'f'})
-    
+    @caregiver_requests = User.find(:all, :conditions => {:household_id => current_user.household_id, :household_confirmed => false})
     respond_to do |format|
       for pending_request in @pending_requests_confirmed
         pending_request.read = true
