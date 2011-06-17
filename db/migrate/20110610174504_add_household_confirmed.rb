f748b0e30cfe79969aee300366a0b85ea663be6f
Chad McKenna <cmckenna@mines.edu>
2011-06-17 14:01:55 -0600

    Changes

--- db/migrate/20110610174504_add_household_confirmed.rb
+++ db/migrate/20110610174504_add_household_confirmed.rb
@@ -1,6 +1,6 @@
 class AddHouseholdConfirmed < ActiveRecord::Migration
   def self.up
-    add_column :users, :household_confirmed, :boolean, :default => false
+    add_column :users, :household_confirmed, :string, :default => false
   end
 
   def self.down
--- db/migrate/20110613170636_change_column_type_for_household_confirmed.rb
+++ db/migrate/20110613170636_change_column_type_for_household_confirmed.rb
@@ -1,15 +1,15 @@
 class ChangeColumnTypeForHouseholdConfirmed < ActiveRecord::Migration
   def self.up
-    add_column :users, :convert_household_confirmed, :boolean, :default => false
+    #add_column :users, :convert_household_confirmed, :boolean, :default => false
     
-    User.reset_column_information
-    User.all.each do |u|
-      u.convert_household_confirmed = u.household_confirmed == '1'
-      u.save
+    #User.reset_column_information
+    #User.all.each do |u|
+    #  u.convert_household_confirmed = u.household_confirmed == '1'
+    #  u.save
     end
     
-    remove_column :users, :household_confirmed
-    rename_column :users, :convert_household_confirmed, :household_confirmed
+    #remove_column :users, :household_confirmed
+    #rename_column :users, :convert_household_confirmed, :household_confirmed
   end
 
   def self.down
