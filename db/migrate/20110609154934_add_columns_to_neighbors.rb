204d09a078046a89ecc489520844cb8cba1099f7
Chad McKenna <cmckenna@mines.edu>
2011-06-17 13:59:15 -0600

    Changes

--- db/migrate/20110609154934_add_columns_to_neighbors.rb
+++ db/migrate/20110609154934_add_columns_to_neighbors.rb
@@ -1,6 +1,6 @@
 class AddColumnsToNeighbors < ActiveRecord::Migration
   def self.up
-    add_column :neighbors, :household_confirmed, :string, :default => false
+    add_column :neighbors, :household_confirmed, :boolean, :default => false
     add_column :neighbors, :neighbor_confirmed, :string, :default => false
   end
 
